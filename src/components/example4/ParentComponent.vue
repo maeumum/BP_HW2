e3203b1032f583203dbf1cd5ecf2591da6ae0b87
maeumum <maeum0707@gmail.com>
2025-11-08 11:09:30 +0900

    E06 제수정

--- src/components/example4/ParentComponent.vue
+++ src/components/example4/ParentComponent.vue
@@ -4,6 +4,13 @@
   </div>
 </template>
 
+<script>
+// 컴포넌트 이름을 명시적으로 정의하는 별도의 스크립트 블록을 추가합니다.
+export default {
+  name: "E06ParentComponent",
+};
+</script>
+
 <script setup>
 import { provide } from 'vue';
 import ChildComponent1 from "@/components/example4/ChildComponent1.vue";
